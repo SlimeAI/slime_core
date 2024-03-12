d0960849a40c5e2c5d52aff7c7bcf5a0a49c2f9d
johncaged <liuzikang0625@gmail.com>
2024-03-12 21:56:50 +0800

    feat: ``set_core_logger``.

--- src/slime_core/logging/logger.py
+++ src/slime_core/logging/logger.py
@@ -1,12 +1,13 @@
 """
 Core logger module.
 """
+import sys
 import logging
+from contextlib import ContextDecorator
 from slime_core.utils.typing import (
     Any,
     NOTHING
 )
-import sys
 
 
 def _create_core_logger() -> logging.Logger:
@@ -34,19 +35,42 @@ core_logger = _default_core_logger
 # Other logger utils
 #
 
-class set_core_logger:
+class set_core_logger(ContextDecorator):
+    """
+    Change the core logger. Can be used as a context manager or a 
+    function decorator, with the previous value restored when exit 
+    or the function ends.
+    """
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, logger: logging.Logger) -> None:
+        global core_logger
+        self.prev = core_logger
+        core_logger = logger
+        # Cache ``logger`` for ``_recreate_cm``.
+        self.logger = logger
+    
+    def __enter__(self) -> None: pass
+    
+    def __exit__(self, *args, **kwargs) -> None:
+        global core_logger
+        core_logger = self.prev
+    
+    def _recreate_cm(self):
+        return self.__class__(self.logger)
 
 #
 # Logger Func Arg Adapter
+# NOTE: Should be put at the end of the file to avoid circular import.
 #
 
 from slime_core.utils.base import BaseDict
 
 
 class LoggerKwargs(BaseDict[str, Any]):
+    """
+    A logger kwargs adapter that removes unsupported params according to 
+    different Python versions.
+    """
     
     def __init__(self, **kwargs):
         # ``stacklevel`` argument was added after py3.8
